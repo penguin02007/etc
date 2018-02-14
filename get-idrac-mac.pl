6792ecff998c8ee592f3f2f195f6eb0f443f4255
lchan <ka.chan@rakops.com>
2018-02-14 16:15:23 -0500

    need to parse ethernet and mac address from racdump

---
+++ get-idrac-mac.pl
@@ -0,0 +1,13 @@
+#! /usr/bin/env perl
+use v5.014;
+use Net::SSH::Perl;
+
+for my $ip ( 137..138 ) {
+    my $ssh = Net::SSH::Perl->new("172.20.230.137");
+    $ssh->login("root", "calvin");
+    my $idrac_cmd = "racadm racdump";
+    my($stdout, $stderr, $exit) = $ssh->cmd("$idrac_cmd");
+    while ($stdout =~ m/MAC|Ethernet/) {
+        say $_;
+    }
+}
